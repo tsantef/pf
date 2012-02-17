7b976b2589fb02d9607c48b2d105fe971ccf42b6
John-Michael Glenn <phyre19@gmail.com>
2012-02-17 15:51:57 -0800

    Some code fixes and doc improvements.

--- lib/shell.php
+++ lib/shell.php
@@ -1,17 +1,15 @@
 <?php
 
+# Run a shell command
 function execute($cmd, &$output = null) {
-    $output_array = null;
-    $exit_code = null;
-
-    exec($cmd, $output_array, $exit_code);
-
+    exec($cmd, $output_array = null, $exit_code = null);
     $output = join("\n", $output_array);
     return $exit_code;
 }
 
+# Create a temporary folder
 function temp_folder() {
-    $tempfile=tempnam(__FILE__,'');
+    $tempfile=tempnam(__FILE__, '');
     $path = realpath($tempfile);
     if (file_exists($tempfile)) {
         unlink($tempfile);
@@ -19,31 +17,36 @@ function temp_folder() {
     return $path;
 }
 
-function cp_r($src, $dst) { 
-    $dir = opendir($src); 
-    @mkdir($dst); 
-    while(false !== ( $file = readdir($dir)) ) { 
-        if (( $file != '.' ) && ( $file != '..' )) { 
-            if ( is_dir($src . '/' . $file) ) { 
-                cp_r($src . '/' . $file,$dst . '/' . $file); 
-            } 
-            else { 
-                copy($src . '/' . $file,$dst . '/' . $file); 
-            } 
-        } 
-    } 
-    closedir($dir); 
-} 
+# Copy a folder recursively
+function cp_r($src, $dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(false !== ( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src.'/'.$file) ) {
+                cp_r($src.'/'.$file, $dst.'/'.$file);
+            } else {
+                copy($src.'/'.$file, $dst.'/'.$file);
+            }
+        }
+    }
+    closedir($dir);
+}
 
-function rm_rf($dir) { 
-   if (is_dir($dir)) { 
-     $objects = scandir($dir); 
-     foreach ($objects as $object) { 
-       if ($object != "." && $object != "..") { 
-         if (filetype($dir."/".$object) == "dir") rm_rf($dir."/".$object); else unlink($dir."/".$object); 
-       } 
-     } 
-     reset($objects); 
-     rmdir($dir); 
-   } 
- } 
+# Remove a directory recursively
+function rm_rf($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != '.' && $object != '..') {
+                if (filetype($dir.'/'.$object) == "dir") {
+                    rm_rf($dir.'/'.$object);
+                } else {
+                    unlink($dir.'/'.$object);
+                }
+            }
+        }
+        reset($objects);
+        rmdir($dir);
+    }
+}
