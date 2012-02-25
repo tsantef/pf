7930d32058d3a8699df81ff5abcbb39696d2035f
John-Michael Glenn <phyre19@gmail.com>
2012-02-24 18:48:55 -0800

    Oops, forgot something

--- lib/loader.php
+++ lib/loader.php
@@ -6,13 +6,3 @@ define("LIB_PATH", basename(dirname(__FILE__)).'/');
 foreach (glob(LIB_PATH."deps/*.php") as $dep) {
     require_once $dep;
 }
-
-function command_loader($class) {
-    $path = LIB_PATH."commands/".$class;
-    if (file_exists($path)) {
-        echo $path.PHP_EOL;
-        require_once $path;
-    }
-}
-
-spl_autoload_register('command_loader');
