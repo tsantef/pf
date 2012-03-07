c99feafff1e478189ced0cb8580b706125af6f68
John-Michael Glenn <phyre19@gmail.com>
2012-03-06 17:38:45 -0800

    General cleanup and removal of dead code

--- lib/deps/shell.php
+++ lib/deps/shell.php
@@ -1,11 +1,11 @@
 <?php
 
 # Run a shell command
-function execute($cmd, &$output=null) {
+function execute($cmd, &$output = null) {
     $output_array = null;
     $exit_code = null;
     exec($cmd, $output_array, $exit_code);
-    if (is_array($output_array)) { 
+    if (is_array($output_array)) {
         $output = join("\n", $output_array);
     }
     return $exit_code;
@@ -58,31 +58,13 @@ function rm_rf($dir) {
 # Prompt for input
 function prompt($msg, $pw = false) {
     echo "$msg";
-    if ($pw == true) { system('stty -echo'); }
+    if ($pw == true) {
+        system('stty -echo');
+    }
     $input = trim(fgets(fopen('php://stdin', 'r')));
-    if ($pw == true) { system('stty echo'); echo PHP_EOL; }
-    return $input; 
+    if ($pw == true) {
+        system('stty echo');
+        echo PHP_EOL;
+    }
+    return $input;
 }
-
-    // function prompt($prompt, $pw = false) {
-    //     # If client is using Windows OS
-    //     // if (preg_match('/^win/i', PHP_OS)) {
-    //     //     $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
-    //     //     file_put_contents($vbscript, 'wscript.echo(InputBox("'.addslashes($prompt).'", "", "password here"))');
-    //     //     $command = "cscript //nologo " . escapeshellarg($vbscript);
-    //     //     $password = rtrim(shell_exec($command));
-    //     //     unlink($vbscript);
-    //     //     return $password;
-    //     // } else {
-    //     //     # IF *nix-based
-    //     //     $command = "/usr/bin/env bash -c 'echo OK'";
-    //     //     if (rtrim(shell_exec($command)) !== 'OK') {
-    //     //         trigger_error("Can't invoke bash");
-    //     //         return;
-    //     //     }
-    //     //     $command = "/usr/bin/env bash -c 'read -s -p \"".addslashes($prompt)."\" mypassword && echo \$mypassword'";
-    //     //     $password = rtrim(shell_exec($command));
-    //     //     echo PHP_EOL;
-    //     //     return $password;
-    //     // }
-    // }
